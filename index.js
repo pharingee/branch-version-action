8108486b242eb9e824c964ebc1968d61aafcbe1f
Gerald Prince Pharin <gerald@pharin.me>
2020-04-11 21:08:13 +0000

    Uncomment code

--- index.js
+++ index.js
@@ -1,49 +1,48 @@
 const core = require("@actions/core");
-// const github = require("@actions/github");
+const github = require("@actions/github");
 
-// const getVersion = (file) => file.match(/\d\.\d\.\d/);
+const getVersion = (file) => file.match(/\d\.\d\.\d/);
 
-// const getFile = async (octokit, issue, path, ref) => {
-//   const { owner, repo } = issue;
-//   const result = await octokit.repos.getContents({
-//     owner,
-//     repo,
-//     path,
-//     ref,
-//   });
+const getFile = async (octokit, issue, path, ref) => {
+  const { owner, repo } = issue;
+  const result = await octokit.repos.getContents({
+    owner,
+    repo,
+    path,
+    ref,
+  });
 
-//   return Buffer.from(result.data.content, "base64").toString();
-// };
+  return Buffer.from(result.data.content, "base64").toString();
+};
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
     core.warning("Starting run");
     const path = core.getInput("versionFile");
-    core.warning(path);
-    // const { context } = github;
-    // const { payload, issue } = context;
-    // core.debug(
-    //   JSON.stringify(payload, undefined, 2),
-    //   JSON.stringify(context, undefined, 2)
-    // );
-    // const { ref } = payload.pull_request.head;
-    // const myToken = core.getInput("githubToken");
-    // const octokit = new github.GitHub(myToken);
+    const { context } = github;
+    const { payload, issue } = context;
+    core.warning(
+      JSON.stringify(payload, undefined, 2),
+      JSON.stringify(context, undefined, 2)
+    );
+    const { ref } = payload.pull_request.head;
+    const myToken = core.getInput("githubToken");
+    const octokit = new github.GitHub(myToken);
 
-    // const branchFile = await getFile(octokit, issue, path, ref);
-    // const branchVersion = getVersion(branchFile);
-    // core.debug(`PR Branch version is ${branchVersion}`);
+    const branchFile = await getFile(octokit, issue, path, ref);
+    const branchVersion = getVersion(branchFile);
+    core.warning(`PR Branch version is ${branchVersion}`);
 
-    // const masterFile = await getFile(octokit, issue, path);
-    // const masterVersion = getVersion(masterFile);
-    // core.debug(`Master Branch version is ${masterVersion}`);
+    const masterFile = await getFile(octokit, issue, path);
+    const masterVersion = getVersion(masterFile);
+    core.warning(`Master Branch version is ${masterVersion}`);
 
-    // const hasChanges = true;
+    const hasChanges = true;
 
-    // if (hasChanges && masterVersion === branchVersion) {
-    //   throw Error(`Please update the version number when changes are made.`);
-    // }
+    if (hasChanges && masterVersion === branchVersion) {
+      throw Error(`Please update the version number when changes are made.`);
+    }
 
     core.debug("Version number updated");
   } catch (error) {
