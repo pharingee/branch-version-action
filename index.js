c0a329d0d10f6922694008d89972fd4c948da051
Gerald Prince Pharin <gerald@pharin.me>
2020-04-11 22:06:02 +0000

    Use setfailed

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const github = require("@actions/github");
 
 const getVersion = (file) => file.match(/\d\.\d\.\d/);
 
-const getBranch = (ref) => {
-  const splitText = ref.split("refs/head/").filter(Boolean);
-  if (splitText.length > 0) return splitText[0];
-  return "";
-};
-
 const getFile = async (octokit, issue, path, ref) => {
   const { owner, repo } = issue;
   const options = ref ? { owner, repo, path, ref } : { owner, repo, path };
@@ -24,23 +18,20 @@ async function run() {
     const path = core.getInput("versionFile");
     const { context } = github;
     const { payload, issue } = context;
-    const ref = getBranch(payload.ref);
 
     const myToken = core.getInput("githubToken");
     const octokit = new github.GitHub(myToken);
 
-    const branchFile = await getFile(octokit, issue, path, ref);
+    const branchFile = await getFile(octokit, issue, path, payload.ref);
     const branchVersion = getVersion(branchFile);
-    core.info(`Version for ${ref} is ${branchVersion}`);
+    core.info(`Version for ${payload.ref} is ${branchVersion}`);
 
     const masterFile = await getFile(octokit, issue, path);
     const masterVersion = getVersion(masterFile);
     core.info(`Master Branch version is ${masterVersion}`);
 
-    const hasChanges = true;
-
-    if (hasChanges && masterVersion === branchVersion) {
-      throw Error(`Please update the version number when changes are made.`);
+    if (masterVersion == branchVersion) {
+      core.setFailed(`Please update the version number when changes are made.`);
     }
 
     core.info("Version number updated");
